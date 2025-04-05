860bf16896b400639f791ab720e041f48966c231
supostus <speeeert@gmail.com>
2025-04-05 17:27:28 +0300

    День 2: исправлен путь к pitchmaster.db и проверка зависимостей

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
 from flask import Flask
 import logging
-from extensions import db, login_manager  # Импорт из нового файла
+from extensions import db, login_manager
 from routes.auth import auth_bp
+from routes.test import test_bp
+from models import User
+import os  # Добавь этот импорт
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pitchmaster.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'pitchmaster.db')  # Явный путь
 
-# Инициализация расширений
 db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
-# Настройка логирования
 logging.basicConfig(filename='pitchmaster.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 logging.info('Приложение PitchMaster запущено')
 
-# Подключение маршрутов
 app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(test_bp, url_prefix='/test')
 
-# Импорт моделей после инициализации
 from models import User, Objection
 
-if __name__ == '__main__':
+def create_test_user():
     with app.app_context():
-        db.create_all()  # Создаёт таблицы в базе данных
+        db.drop_all()
+        db.create_all()
+        if not User.query.filter_by(email='test@example.com').first():
+            test_user = User(email='test@example.com', password='test123')
+            db.session.add(test_user)
+            db.session.commit()
+            logging.info('Тестовый пользователь создан')
+
+if __name__ == '__main__':
+    create_test_user()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
